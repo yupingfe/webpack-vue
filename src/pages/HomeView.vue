1b8132324021b43dc3010bbe8b6d6ea89334af41
YUPING <70244232+yupingfe@users.noreply.github.com>
2023-09-02 15:12:11 +0000

    feat: webpack config done

---
+++ src/pages/HomeView.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+    <h1>hello world</h1>
+<h1>{{counter}}</h1>
+<button @click="doAdd">add</button>
+</div>
+</template>
+
+<script>
+
+export default {
+name: 'App',
+data() {
+    return {
+        counter: 1
+    }
+},
+created() {
+console.log('created!!')
+},
+methods: {
+    doAdd() {
+        this.counter = ++this.counter
+    }
+}
+}
+</script>
+
+<style lang="less" scoped>
+.h1 {
+    color: #ff0000;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,28 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import HomeView from "../pages/HomeView.vue";
+// import AboutView from "../pages/AboutView.vue";
+
+Vue.use(VueRouter);
+
+let routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  }
+];
+
+if(false) {
+  routes.push(  {
+    path: "/about",
+    name: "about",
+    component: () => import(/* webpackChunkName: 'about' */ '../pages/AboutView.vue'),
+  })
+}
+
+const router = new VueRouter({
+  routes,
+});
+
+export default router;
